4f65b33ee7a5f9aa86f9bcb81fdedad1c6d0f43a
meowlena <milenabraga98@gmail.com>
2024-12-29 21:56:06 -0300

    fixing identation

--- factorial.lua
+++ factorial.lua
@@ -1,12 +1,12 @@
 -- defines a factorial function
 function fact (n)
-    if n == 0 then
-      return 1
-    else
-      return n * fact(n-1)
-    end
+  if n == 0 then
+    return 1
+  else
+    return n * fact(n-1)
   end
+end
   
-  print("enter a number:")
-  num = io.read("*number")        -- read a number
-  print(fact(num))
+print("enter a number:")
+num = io.read("*number")        -- read a number
+print(fact(num))
